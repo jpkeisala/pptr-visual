2be3e6c56b2e311a7d564534226dfccdd2f4c739
Jukka-Pekka Keisala <jpkeisala@outlook.com>
2021-08-25 12:29:40 +0200

    Remove Element Snapshot

--- tests/__tests_/visual.test.js
+++ tests/__tests_/visual.test.js
@@ -65,6 +65,20 @@ describe('Visual Regression Testing', () => {
             )
         })
 
+        test('Remove Element Snapshot', async function(){
+            await page.goto('https://example.com')
+            await page.waitForSelector('h1')
+            await page.evaluate(() => {
+                ;(document.querySelectorAll('h1') || []).forEach(el => el.remove())
+            })
+            await page.emulate(puppeteer.devices['iPad landscape'])
+            const image = await page.screenshot()
+            expect(image).toMatchImageSnapshot({
+                failureThresholdType: 'percent',
+                failureThreshold: 0.01,
+            }    
+            )
+        })
 })
 
 
